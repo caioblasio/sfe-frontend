e4924261c7f6119bb792d26ac7641cbd1f9c041b
Caio De Blasio <caio.de-blasio@aklamio.com>
2020-04-19 22:08:58 +0200

    created stepper component

---
+++ app/components/Stepper/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+
+const TopStepper = ({ activeStep }) => {
+  // const classes = useStyles();
+  return (
+    <Stepper activeStep={activeStep} alternativeLabel>
+      {STEPS.map((label) => (
+        <Step key={label}>
+          <StepLabel>{label}</StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  );
+};
+
+export default TopStepper;
